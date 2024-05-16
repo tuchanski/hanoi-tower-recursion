6addeae8bb3941871a066ec732d86bf774e09aad
Tuchanski <gui.rocha2004@gmail.com>
2024-05-16 15:19:02 -0300

    Fixed Hanoi Algorithm by Recursion.

--- hanoi.py
+++ hanoi.py
@@ -3,16 +3,11 @@ class Hanoi:
     def __init__(self) -> None:
         pass
 
-    def game(self, n, first, second, third):
-
-        # Caso base: Se não houver discos para mover, não faz nada.
-        if n == 0:
-            return 0
-        # Move (n-1) discos da haste de origem para a haste auxiliar, usando a haste de destino como auxiliar.
-        moves = self.game(n - 1, first, third, second)
-        # Move (n-1) discos da haste auxiliar para a haste de destino, usando a haste de origem como auxiliar.
-        moves += 1 + self.game(n - 1, second, first, third)
-        return moves
-
-    def min_moves(self, n):
-        return self.game(n, 'A', 'B', 'C')
+    def min_moves(self, n: int) -> int:
+        if n < 0:
+            raise ValueError("Number of disks cannot be negative")
+        elif n == 1:
+            return 1
+        else:
+            # Fórmula -> T(n) = 2n − 1
+            return 2 * self.min_moves(n - 1) + 1
