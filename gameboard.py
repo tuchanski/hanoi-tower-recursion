7f3b0abc9f77ad685ddd8b000094980240d56d54
Tuchanski <gui.rocha2004@gmail.com>
2024-06-15 16:53:32 -0300

    Reestruturada aplicação

---
+++ gameboard.py
@@ -0,0 +1,26 @@
+class Hanoi:
+
+    def __init__(self) -> None:
+        pass
+
+    def min_moves(self, n: int) -> int:
+        if n < 0:
+            raise ValueError("Number of disks cannot be negative")
+        elif n == 1:
+            return 1
+        else:
+            return 2 * self.min_moves(n - 1) + 1
+
+class Menu:
+    def __init__(self) -> None:
+        pass
+
+    def show(self):
+        print("\n- Hanoi Tower | Min. Moves Calculator -\n[1] - Custom value\n[2] - Default (7 disks)\n[0] - Exit")
+
+    def selection(self):
+        try:
+            mode =  int(input("Choose here: "))
+            return mode
+        except ValueError:
+            pass
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from menu import Menu
-from hanoi import Hanoi
-
+from gameboard import *
 def main():
 
     menu = Menu()
-    hanoi = Hanoi()
+    gameboard = Hanoi()
 
     while True:
+
         menu.show()
-        mode = menu.select_mode()
+        mode = menu.selection()
+
         try:
             if mode == 1:
-                n = int(input("\n- Enter number of disks: "))
-                print(f"- Min. number of moves for {n} disks:", hanoi.min_moves(n))
+                disk_value = int(input("\n- Enter number of disks: "))
+                print(f"- Min. number of moves for {disk_value} disks:", gameboard.min_moves(disk_value))
             elif mode == 2:
-                print(f"\n- Min. number of moves for 7 disks:", hanoi.min_moves(7))
+                print(f"\n- Min. number of moves for 7 disks:", gameboard.min_moves(7))
             elif mode == 0:
                 print("\nProgram has been finished!")
                 break
             else:
-                print("\nError: Invalid input")
+                print("\nThis isn't an option!\nValue must be in range 0-2.")
         except ValueError:
             print("\nError: Invalid input. Please enter a valid integer.")
         except RecursionError:
